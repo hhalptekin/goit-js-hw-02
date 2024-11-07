42731253196dec63a6d814d52646b0e0680a01be
Hasan Hüseyin ALPTEKİN <hsn.hsyn.alptekin@gmail.com>
2024-11-07 10:20:46 +0300

    task 2 done

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,12 @@
-function formatMessage(message, maxLength){
+function formatMessage(message, maxLength) {
 
+    if (message.length == maxLength || message.length < maxLength) {
+        return message;
+    } else {
+        return message.slice(0, maxLength) + '...';
+    }
 }
+
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
 console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
 console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
